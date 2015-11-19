53f9aaaad7cf0e5150cfb62f119341eb8bdd10e9
Aleksandar Balalovski <abalalovski@gmail.com>
2015-11-19 16:23:09 +0100

    Test fixed

--- app/src/test/java/com/twodwarfs/shutterstock/MainTest.java
+++ app/src/test/java/com/twodwarfs/shutterstock/MainTest.java
@@ -1,6 +1,12 @@
 package com.twodwarfs.retrofitoo;
 
+import android.test.suitebuilder.annotation.SmallTest;
+
 import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.lang.Exception;
+import java.lang.Override;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +14,19 @@ import static org.junit.Assert.*;
  * Api Test, I don't know what other can be tested in this sort of app
  */
 
-public class ApiTest {
+public class ApiTest extends TestCase {
 
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @SmallTest
     public void testApiObject() {
         Call<ShutterResponse> call = ShutterApi.getInstance().getPhotos();
         assertNotNull(call);
