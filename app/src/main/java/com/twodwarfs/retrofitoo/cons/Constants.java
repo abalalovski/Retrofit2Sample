12f1133db28d07c1e8dd32a66088962afe9552fe
Aleksandar Balalovski <abalalovski@gmail.com>
2015-11-19 16:06:36 +0100

    Constants

--- app/src/main/java/com/twodwarfs/retrofitoo/cons/Constants.java
+++ app/src/main/java/com/twodwarfs/retrofitoo/cons/Constants.java
@@ -6,7 +6,7 @@ package com.twodwarfs.retrofitoo.cons;
 public class Constants {
 
     public static final String TAG = "ShutterstockApp";
-    public static final String CLIENT_ID = "9847e6f8cab8e2a1ebb3";
-    public static final String CLIENT_SECRET = "c054069df1279844e2c8605db0865f3f3a676e26";
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
     public static final String BASE_URL = "https://api.shutterstock.com";
 }
